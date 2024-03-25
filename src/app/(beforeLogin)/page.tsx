f95ca88b2b5730988cb30a4099cf4ed2979fc143
hongyeojung <hongshii0i0i@gmail.com>
2024-03-25 18:05:45 +0900

    240325 클론코딩 전 세팅 완료

--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -1,7 +1,3 @@
-import Image from "next/image";
-import styles from "@/app/page.module.css";
-import Link from "next/link";
-import zLogo from "../../../public/zlogo.png"
 import Main from "./_component/Main";
 
 export default function Home() {
